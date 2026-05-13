ae5ef82a10dd902560b0d9441efa9c2d1104e591
sivasriram-06 <sivasriram.balasubramaniyan@sunoida.com>
2026-05-13 15:50:23 +0530

    'host'

--- flask/server.py
+++ flask/server.py
@@ -64,6 +64,8 @@ def get_all_syms():
     return list(get_all_symptoms())
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
     print("Flask app running")
-    app.run()
+    app.run(host="0.0.0.0", port=port)
 
